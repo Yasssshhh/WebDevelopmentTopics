26d8cf7cb3bc836d3edfe67d9284031202b37bca
Yash Pathak <yashpathak6302@gmail.com>
2023-05-04 18:41:15 +0530

    DOM

---
+++ JavaScript/11.DOM/Attributes/app.js
@@ -0,0 +1,9 @@
+// in a console
+const div1 = document.getElementById("div1");
+//=> <div id="div1">Hi Champ!</div>
+
+const exampleAttr = div1.getAttribute("id");
+//=> "div1"
+
+const align = div1.getAttribute("align");
+//=> null
